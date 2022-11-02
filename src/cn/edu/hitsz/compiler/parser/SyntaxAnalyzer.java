5da72b6d6fa1c86c326ff5b88083e9a7be6db973
LQ_laptop <1264432729@qq.com>
2022-11-02 11:52:40 +0800

    extract Symbol

--- src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
@@ -160,31 +160,4 @@ public class SyntaxAnalyzer {
             }
         }
     }
-
-    class Symbol{
-        Token token;
-        NonTerminal nonTerminal;
-
-        private Symbol(Token token, NonTerminal nonTerminal){
-            this.token = token;
-            this.nonTerminal = nonTerminal;
-        }
-
-        public Symbol(Token token){
-            this(token, null);
-        }
-
-        public Symbol(NonTerminal nonTerminal){
-            this(null, nonTerminal);
-        }
-
-        public boolean isToken(){
-            return this.token != null;
-        }
-
-        public boolean isNonterminal(){
-            return this.nonTerminal != null;
-        }
-    }
-
 }
