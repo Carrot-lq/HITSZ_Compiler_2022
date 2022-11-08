cfa5340b64346faa9f4e214afaaff96e9f26779a
LQ_laptop <1264432729@qq.com>
2022-11-08 18:57:42 +0800

    Lab4

---
+++ data/out/assembly_language.asm
@@ -0,0 +1,13 @@
+.text
+	li t0,8	# (MOV, a, 8)
+	li t1,5	# (MOV, b, 5)
+	li t2,3	# (MOV, $6, 3)
+	sub t3,t2,t0	# (SUB, $0, $6, a)
+	mv t4,t3	# (MOV, c, $0)
+	mul t5,t0,t1	# (MUL, $1, a, b)
+	addi t6,t1,3	# (ADD, $2, b, 3)
+	sub t2,t4,t0	# (SUB, $3, c, a)
+	mul t0,t6,t2	# (MUL, $4, $2, $3)
+	sub t2,t5,t0	# (SUB, $5, $1, $4)
+	mv t0,t2	# (MOV, result, $5)
+	mv a0,t0	# (RET, , result)
--- src/cn/edu/hitsz/compiler/asm/AssemblyGenerator.java
+++ src/cn/edu/hitsz/compiler/asm/AssemblyGenerator.java
@@ -1,13 +1,14 @@
 package cn.edu.hitsz.compiler.asm;
 
-import cn.edu.hitsz.compiler.NotImplementedException;
-import cn.edu.hitsz.compiler.ir.Instruction;
+import cn.edu.hitsz.compiler.ir.*;
+import cn.edu.hitsz.compiler.utils.FileUtils;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 /**
- * TODO: 实验四: 实现汇编生成
+ * 实验四: 实现汇编生成
  * <br>
  * 在编译器的整体框架中, 代码生成可以称作后端, 而前面的所有工作都可称为前端.
  * <br>
@@ -22,6 +23,49 @@ import java.util.List;
  */
 public class AssemblyGenerator {
 
+    // 预处理后指令列表
+    List<Instruction> instList = new LinkedList<>();
+    // 变量与寄存器双向map
+    BMap<IRValue, Register> variableMap = new BMap<>();
+    // 汇编代码，初始化第一行为 ".text"
+    List<String> sentences = new ArrayList<>(List.of(".text"));
+    // 可以分配的寄存器号
+    enum Register {
+        t0,t1,t2,t3,t4,t5,t6
+    }
+
+    public void addVariable(IRValue operand, int instIndex){
+        // 立即数无需分配寄存器
+        if (operand.isImmediate()) {
+            return;
+        }
+        // 已存入变量，无需再分配
+        if (variableMap.containsKey(operand)) {
+            return;
+        }
+        // 未存入，先寻找空闲寄存器
+        for (Register reg : Register.values()) {
+            if(!variableMap.containsValue(reg)){
+                variableMap.replace(operand,reg);
+                return;
+            }
+        }
+        // 若均不空闲，寻找后续不再使用的变量占用的寄存器
+        Set<Register> cleanableRegs = Arrays.stream(Register.values()).collect(Collectors.toSet());
+        for(int i = instIndex; i<instList.size(); i++){
+            // 排除后续每一条指令所有出现的变量所占用的寄存器（若存在），剩余即为可被清理的寄存器
+            Instruction inst = instList.get(i);
+            for(IRValue irValue : inst.getOperands()){
+                Register reg = variableMap.getByKey(irValue);
+                cleanableRegs.remove(reg);
+            }
+        }
+        // 存在可清理寄存器则将其分配
+        if(!cleanableRegs.isEmpty()) {
+            variableMap.replace(operand,cleanableRegs.iterator().next());
+        }
+    }
+
     /**
      * 加载前端提供的中间代码
      * <br>
@@ -31,10 +75,69 @@ public class AssemblyGenerator {
      * @param originInstructions 前端提供的中间代码
      */
     public void loadIR(List<Instruction> originInstructions) {
-        // TODO: 读入前端提供的中间代码并生成所需要的信息
-        throw new NotImplementedException();
+        for (Instruction inst : originInstructions) {
+            switch (inst.getKind()) {
+                // 两个操作数的指令
+                case ADD,SUB,MUL -> {
+                    // 预处理操作数有立即数的指令
+                    boolean lhsIsImm = inst.getLHS().isImmediate();
+                    boolean rhsIsImm = inst.getRHS().isImmediate();
+                    if (lhsIsImm && rhsIsImm) {
+                        // 两个立即数直接求值得到结果
+                        InstructionKind kind = inst.getKind();
+                        int l = ((IRImmediate)inst.getLHS()).getValue();
+                        int r = ((IRImmediate)inst.getRHS()).getValue();
+                        int res = switch (kind) {
+                            case ADD -> l + r;
+                            case SUB -> l - r;
+                            case MUL -> l * r;
+                            default -> 0;
+                        };
+                        IRImmediate tmp = IRImmediate.of(res);
+                        instList.add(Instruction.createMov(inst.getResult(),tmp));
+                    } else if (lhsIsImm && !rhsIsImm) {
+                        // 左立即数修改指令
+                        switch (inst.getKind()) {
+                            // 加法交换左立即数至右边
+                            case ADD -> instList.add(Instruction.createAdd(inst.getResult(),inst.getRHS(),inst.getLHS()));
+                            // 减法与乘法指令前添加 MOV tmp imm
+                            case SUB -> {
+                                IRVariable tmp = IRVariable.temp();
+                                instList.add(Instruction.createMov(tmp,inst.getLHS()));
+                                instList.add(Instruction.createSub(inst.getResult(),tmp,inst.getRHS()));
+                            }
+                            case MUL -> {
+                                IRVariable tmp = IRVariable.temp();
+                                instList.add(Instruction.createMov(tmp,inst.getLHS()));
+                                instList.add(Instruction.createMul(inst.getResult(),tmp,inst.getLHS()));
+                            }
+                            default -> instList.add(inst);
+                        }
+                    } else if (!lhsIsImm && rhsIsImm) {
+                        // 右立即数修改指令
+                        // 乘法指令前添加 MOV tmp IMM
+                        if (inst.getKind() == InstructionKind.MUL) {
+                            IRVariable tmp = IRVariable.temp();
+                            instList.add(Instruction.createMov(tmp,inst.getLHS()));
+                            instList.add(Instruction.createMul(inst.getResult(),tmp,inst.getLHS()));
+                        } else {
+                            instList.add(inst);
+                        }
+                    } else {
+                        // 两个操作数均不为立即数，无需处理
+                        instList.add(inst);
+                    }
+                }
+                // 一个操作数的指令
+                case RET -> {
+                    // 遇到RET指令后直接舍弃后续指令
+                    instList.add(inst);
+                    return;
+                }
+                case MOV -> instList.add(inst);
+            }
+        }
     }
-
 
     /**
      * 执行代码生成.
@@ -46,8 +149,84 @@ public class AssemblyGenerator {
      * 成前完成建立, 与代码生成的过程相关的信息可自行设计数据结构进行记录并动态维护.
      */
     public void run() {
-        // TODO: 执行寄存器分配与代码生成
-        throw new NotImplementedException();
+        // 执行寄存器分配与代码生成
+        for(int i=0;i<instList.size();i++) {
+            Instruction inst = instList.get(i);
+            String str = null;
+            switch (inst.getKind()) {
+                // 对ADD SUB MUL，为三个变量分配寄存器（若需要），并根据是否含立即数生成对应汇编代码
+                case ADD -> {
+
+                    IRValue res = inst.getResult();
+                    IRValue lhs = inst.getLHS();
+                    IRValue rhs = inst.getRHS();
+                    addVariable(res,i);
+                    addVariable(lhs,i);
+                    addVariable(rhs,i);
+                    Register resReg = variableMap.getByKey(res);
+                    Register lhsReg = variableMap.getByKey(lhs);
+                    Register rhsReg = variableMap.getByKey(rhs);
+                    if(rhs.isImmediate()){
+                         str = "\taddi %s,%s,%s".formatted(resReg.toString(),lhsReg.toString(),rhs.toString());
+                    } else {
+                        str = "\tadd %s,%s,%s".formatted(resReg.toString(),lhsReg.toString(),rhsReg.toString());
+                    }
+                }
+                case SUB -> {
+                    IRValue res = inst.getResult();
+                    IRValue lhs = inst.getLHS();
+                    IRValue rhs = inst.getRHS();
+                    addVariable(res,i);
+                    addVariable(lhs,i);
+                    addVariable(rhs,i);
+                    Register resReg = variableMap.getByKey(res);
+                    Register lhsReg = variableMap.getByKey(lhs);
+                    Register rhsReg = variableMap.getByKey(rhs);
+                    if(rhs.isImmediate()){
+                        str = "\tsubi %s,%s,%s".formatted(resReg.toString(),lhsReg.toString(),rhs.toString());
+                    } else {
+                        str = "\tsub %s,%s,%s".formatted(resReg.toString(),lhsReg.toString(),rhsReg.toString());
+                    }
+                }
+                case MUL -> {
+                    IRValue res = inst.getResult();
+                    IRValue lhs = inst.getLHS();
+                    IRValue rhs = inst.getRHS();
+                    addVariable(res,i);
+                    addVariable(lhs,i);
+                    addVariable(rhs,i);
+                    Register resReg = variableMap.getByKey(res);
+                    Register lhsReg = variableMap.getByKey(lhs);
+                    Register rhsReg = variableMap.getByKey(rhs);
+                    str = "\tmul %s,%s,%s".formatted(resReg.toString(),lhsReg.toString(),rhsReg.toString());
+                }
+                // 对MOV，为两个变量分配寄存器（若需要）
+                // 若第二个操作数为立即数，生成汇编代码为 li（加载立即数），否则为 mv
+                case MOV -> {
+                    IRValue res = inst.getResult();
+                    IRValue from = inst.getFrom();
+                    addVariable(res,i);
+                    addVariable(from,i);
+                    Register resReg = variableMap.getByKey(res);
+                    Register fromReg = variableMap.getByKey(from);
+                    if (from.isImmediate()) {
+                        str = "\tli %s,%s".formatted(resReg.toString(),from.toString());
+                    } else {
+                        str = "\tmv %s,%s".formatted(resReg.toString(),fromReg.toString());
+                    }
+                }
+                // 对RET，生成汇编代码为 mv a0 __
+                case RET -> str = "\tmv a0," + variableMap.getByKey(inst.getReturnValue()).toString();
+            }
+            // 添加注释，即对应中间代码
+            str = str + "\t# %s".formatted(inst.toString());
+            sentences.add(str);
+            // 读取到RET指令后，直接舍弃后续指令
+            if (inst.getKind() == InstructionKind.RET) {
+                break;
+            }
+        }
+        System.out.println("Assembly Generate over");
     }
 
 
@@ -57,8 +236,8 @@ public class AssemblyGenerator {
      * @param path 输出文件路径
      */
     public void dump(String path) {
-        // TODO: 输出汇编代码到文件
-        throw new NotImplementedException();
+        // 输出汇编代码到文件
+        FileUtils.writeLines(path,sentences.stream().toList());
     }
 }
 
